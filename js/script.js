a3d2fa7c9f7a255edf8bbb49bd21495812e3c8a6
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-28 20:56:59 +0100

    Working on nav-bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // var
-var userVictory = 5;
+var userVictory = 1;
 
 // code
 $(document).ready(function() {
-  console.log('hello world');
   console.log(userVictory);
+
+  $(document).on('click', '.hamburger', function() {
+    $('.nav-mobile').toggleClass('display-flex');
+  });
+
 });
