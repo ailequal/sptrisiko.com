18b8cb0fbb5be04684d48b12f171459a34e44630
ailequal <37016865+ailequal@users.noreply.github.com>
2020-02-02 13:26:00 +0100

    Fixed nav-mobile overflow problem

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@ var userVictory = 1;
 
 // code
 $(document).ready(function() {
+  
   console.log(userVictory);
 
+  // toggle nav-mobile
   $(document).on('click touchend', '.hamburger', function() {
+    $('body').toggleClass('overflow-hidden');
     $('.nav-mobile').toggleClass('display-flex');
   });
 
